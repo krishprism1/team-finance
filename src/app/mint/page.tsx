11f85147187f1b03218b22f89835d46421d4510a
Ashutoshpython123 <codingcampus111@gmail.com>
2024-08-12 13:31:41 +0530

    set the navbar for action layout

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -6,13 +6,6 @@ export default function Mint() {
     return (
         <ActionLayout>
             <div className="creat-token-container">
-                <div className="back-home-box">
-                    <div className='image-container'>
-                        <img src="https://app.team.finance/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-header.43749e9b.png&w=384&q=75" alt="" />
-                        <a href="#"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="left-icon" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"></path></svg>
-                            <span>Back to home page</span></a>
-                    </div>
-                </div>
                 <div className="creat-token-column">
                     <div className="content1">
                         <h1>Create token</h1>
--- src/containers/ActionLayout.tsx
+++ src/containers/ActionLayout.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 export default function ActionLayout({
-    children
-  }: Readonly<{
-    children: React.ReactNode
-  }>) {
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
     <div>
+      <div className="back-home-box">
+        <div className='image-container'>
+          <img src="https://app.team.finance/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-header.43749e9b.png&w=384&q=75" alt="" />
+          <a href="#"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="left-icon" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.41 16.59 10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"></path></svg>
+            <span>Back to home page</span></a>
+        </div>
+      </div>
       {children}
     </div>
   )
