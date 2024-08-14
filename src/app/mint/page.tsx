25f8d420f0ecde1246533c6a00e9b373bfae05ed
MANSIH KUMAR <manishkimar3456@gmail.com>
2024-08-14 14:27:58 +0530

    created Mint page responsive and all component  Tablet view

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -2,6 +2,7 @@
 import ActionLayout from '@/containers/ActionLayout'
 import React from 'react'
 import "../../styles/mint.css"
+import "../../styles/Mint-responsive.css"
 
 export default function Mint() {
     return (
@@ -70,10 +71,6 @@ export default function Mint() {
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="ds-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
                             </div>
                         </div>
-                        <div>
-                            <a href="#">Select wallet</a>
-                        </div>
-                    </div>
 {/* hide code here */}
                         <div className="connected-wallet-hide-box">
                             <div>
@@ -86,6 +83,10 @@ export default function Mint() {
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="ds-icon2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path></svg>
                             </div>
                         </div>
+                        <div>
+                            <a href="#">Select wallet</a>
+                        </div>
+                    </div>
                 <div className="popup-box">
                     <div className="popup-container">
                         <div className="top-column">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="links">
-                    <div><a href="#">Create vesting<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="arrow-right" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"></path></svg></a></div>
+                    <div><a href="#">Create audited token<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="arrow-right" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"></path></svg></a></div>
                     <div><a href="#">More info <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="arrow-right2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path><path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path></svg></a></div>
                 </div>
             </div>
