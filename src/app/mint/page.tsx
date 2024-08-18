8b5b2fb31e7b2224889c69a6718ba9c2c247fda7
Ashutoshpython123 <codingcampus111@gmail.com>
2024-08-18 11:06:05 +0530

    code document

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -6,6 +6,7 @@ import "../../styles/Mint-responsive.css"
 import { useAccount } from 'wagmi'
 import { useWeb3Modal } from '@web3modal/wagmi/react'
 import { formatEthAddr } from '@/utils/math.utils'
+import ConnectWallet from '@/components/common/create/ConnectWallet'
 
 export default function Mint() {
     const { address } = useAccount()
@@ -16,53 +17,11 @@ export default function Mint() {
     return (
         <ActionLayout>
             <div className="creat-token-container">
-                <div className="creat-token-column">
-                    <div className="content1">
-                        <h1>Create token</h1>
-                        <div>
-                            <a href="#"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="vd-icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m9 17 8-5-8-5z"></path></svg>Watch tutorial</a>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="vd-icon2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m9 17 8-5-8-5z"></path></svg>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="vd-icon3" aria-hidden="true" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"></path></svg>
-                        </div>
-                    </div>
-                </div>
-                <div className="token-creation-bar">
-                    <div className="column">
-                        <button className='btn-radio1'></button>
-                        <div className='content'>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className="radio-bar1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M21 18v1c0 1.1-.9 2-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14c1.1 0 2 .9 2 2v1h-9a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h9zm-9-2h10V8H12v8zm4-2.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"></path></svg>
-                            <span className='span1'>connect wallet</span>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <button className='btn-radio2'></button>
-                        <div className='content'>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className='radio-bar2' height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M13 13v8h8v-8h-8zM3 21h8v-8H3v8zM3 3v8h8V3H3zm13.66-1.31L11 7.34 16.66 13l5.66-5.66-5.66-5.65z"></path></svg>
-                            <span className='span2'>Select blockchain</span>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <button className='btn-radio3'></button>
-                        <div className='content'>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className='radio-bar3' height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path></svg>
-                            <span className='span3'>Enter token info</span>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <button className='btn-radio4'></button>
-                        <div className='content'>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className='radio-bar4' height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M15 16a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6z"></path><path d="M5 16h1V8a2 2 0 0 1 2-2h8V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zm3 3a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-9a2 2 0 0 0-2-2h-1v8a2 2 0 0 1-2 2H8v1z"></path></svg>
-                            <span className='span4'>Add features</span>
-                        </div>
-                    </div>
-                    <div className="column">
-                        <button className='btn-radio5'></button>
-                        <div className='content'>
-                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className='radio-bar5' height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M2.01 21 23 12 2.01 3 2 10l15 2-15 2z"></path></svg>
-                            <span className='span5'>Create contract</span>
-                        </div>
-                    </div>
-                </div>
+
+                <ConnectWallet />
+
+               
+
                 <div className="select-wallet-box">
                     <div className='select-wallet'>
                         <h1>Connect wallet</h1>
@@ -124,6 +83,8 @@ export default function Mint() {
                         </div>
                     </div>
                 </div>
+
+
                 <div className="select-blockchain-box">
                     <div className="select-blockchain">
                         <div className='select-box1'>
@@ -322,7 +283,6 @@ export default function Mint() {
                         </div>
                     </div>
                 </div>
-
             </div>
         </ActionLayout>
     )
