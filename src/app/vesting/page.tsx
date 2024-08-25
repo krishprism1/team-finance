7cf68fde283e91c58ff5453130b6e56cd59de127
MANSIH KUMAR <manishkimar3456@gmail.com>
2024-08-25 17:06:31 +0530

    created vesting page

--- src/app/vesting/page.tsx
+++ src/app/vesting/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react'
 import "../../styles/mint.css"
 import "../../styles/Mint-responsive.css"
 import "../../styles/multisender.css"
+import "../../styles/MultisenderResponsive.css"
+import "../../styles/Staking.css"
 import { useAccount } from 'wagmi'
 import ConnectWallet from '@/components/common/createform/ConnectWallet'
 import SelectNetwork from '@/components/common/createform/SelectNetwork'
@@ -257,6 +259,203 @@ export default function Vesting() {
                                             </div>
                                         </div>
                                     )}
+                            <div>
+                                <div className="recipient-box1 vesting-token-details">
+                   
+                                        <div className="recipient-box2 recipient-box2-2">
+                                            <div className="rct-box2">
+                                                <div className="small-rtc-box">
+                                                    <p>Token balance</p>
+                                                    <span>5.00 OOE</span>
+                                                </div>
+                                                <div className="small-rtc-box">
+                                                    <p>Total vesting amount</p>
+                                                    <span>3 OOE</span>
+                                                </div>
+                                                <div className="small-rtc-box">
+                                                    <p>Fee</p>
+                                                    <span>$0.00</span>
+                                                </div>
+
+                                                <div className="small-rtc-box3">
+                                                    <div className="draw-no-about-token">
+                                                        <div className="fully-details-box">
+                                                            <div>
+                                                                <span>1.</span>
+                                                                <p>0xf90f...eae9</p>
+                                                            </div>
+                                                            <div>
+                                                                <button>
+                                                                <svg stroke="#7992f2" fill="#7992f2" stroke-width="0" viewBox="0 0 24 24" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M4 21a1 1 0 0 0 .24 0l4-1a1 1 0 0 0 .47-.26L21 7.41a2 2 0 0 0 0-2.82L19.42 3a2 2 0 0 0-2.83 0L4.3 15.29a1.06 1.06 0 0 0-.27.47l-1 4A1 1 0 0 0 3.76 21 1 1 0 0 0 4 21zM18 4.41 19.59 6 18 7.59 16.42 6zM5.91 16.51 15 7.41 16.59 9l-9.1 9.1-2.11.52z"></path></svg>
+                                                                </button>
+                                                                <button>
+                                                                <svg stroke="#7992f2" fill="#7992f2" stroke-width="0" viewBox="0 0 24 24"  height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M11 10H9v3H6v2h3v3h2v-3h3v-2h-3z"></path><path d="M4 22h12c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2zM4 8h12l.002 12H4V8z"></path><path d="M20 2H8v2h12v12h2V4c0-1.103-.897-2-2-2z"></path></svg>   
+                                                                </button>
+                                                                <button>
+                                                                    <svg
+                                                                        stroke="#7992f2"
+                                                                        fill="#7992f2"
+                                                                        stroke-width="0"
+                                                                        viewBox="0 0 24 24"
+                                                                        height="1.5em"
+                                                                        width="1.5em"
+                                                                        xmlns="http://www.w3.org/2000/svg"
+                                                                    >
+                                                                        <path d="M15 2H9c-1.103 0-2 .897-2 2v2H3v2h2v12c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V8h2V6h-4V4c0-1.103-.897-2-2-2zM9 4h6v2H9V4zm8 16H7V8h10v12z"></path>
+                                                                    </svg>
+                                                                </button>
+                                                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" className='last-svg-up-arrrow'  height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>
+                                                            </div>
+                                                        </div>
+
+                                                    <div className="details-token-vesting-plan">
+                                                            <div>
+                                                                <p>Number of tokens to vest</p>
+                                                                <span>2</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Wallet nickname</p>
+                                                                <span></span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Relationship</p>
+                                                                <span>Employee</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Start date & time</p>
+                                                                <span>Sep 25, 2024, 2:53 PM UTC</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>End date & time</p>
+                                                                <span>Sep 25, 2024, 2:53 PM UTC</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Vesting cadence</p>
+                                                                <span>Per day</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Cliff length</p>
+                                                                <span>0 Months</span>
+                                                            </div>
+                                                            <div>
+                                                                <p>Release percentage at cliff end</p>
+                                                                <span>0%</span>
+                                                            </div>
+                                                    </div>
+                                                 </div>
+                                                </div>
+                                                <div className="small-rtc-box4">
+                                                    <div className="div1">
+                                                        <div>
+                                                            <p>Showing page 1 of 1</p>
+                                                            <p>1 results</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="div22">
+                                                        <div className="div2">
+                                                            <p>Show rows</p>
+                                                            <select
+                                                                id="mySelect">
+                                                                <option value="1">1</option>
+                                                                <option value="5">5</option>
+                                                                <option value="10">10</option>
+                                                            </select>
+                                                        </div>
+                                                        <div className="div3">
+                                                            <button>
+                                                                <svg
+                                                                    stroke="currentColor"
+                                                                    fill="none"
+                                                                    stroke-width="0"
+                                                                    viewBox="0 0 24 24"
+                                                                    height="20"
+                                                                    width="20"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                >
+                                                                    <path
+                                                                        d="M18.3639 7.75735L16.9497 6.34314L11.2929 12L16.9497 17.6568L18.3639 16.2426L14.1213 12L18.3639 7.75735Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                    <path
+                                                                        d="M11.2929 6.34314L12.7071 7.75735L8.46447 12L12.7071 16.2426L11.2929 17.6568L5.63605 12L11.2929 6.34314Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                </svg>
+                                                            </button>
+                                                            <button>
+                                                                <svg
+                                                                    stroke="currentColor"
+                                                                    fill="none"
+                                                                    stroke-width="0"
+                                                                    viewBox="0 0 24 24"
+                                                                    height="20"
+                                                                    width="20"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                >
+                                                                    <path
+                                                                        d="M16.2426 6.34317L14.8284 4.92896L7.75739 12L14.8285 19.0711L16.2427 17.6569L10.5858 12L16.2426 6.34317Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                </svg>
+                                                            </button>
+                                                            <button>
+                                                                <svg
+                                                                    stroke="currentColor"
+                                                                    fill="none"
+                                                                    stroke-width="0"
+                                                                    viewBox="0 0 24 24"
+                                                                    height="20"
+                                                                    width="20"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                >
+                                                                    <path
+                                                                        d="M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                </svg>
+                                                            </button>
+                                                            <button>
+                                                                <svg
+                                                                    stroke="currentColor"
+                                                                    fill="none"
+                                                                    stroke-width="0"
+                                                                    viewBox="0 0 24 24"
+                                                                    height="20"
+                                                                    width="20"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                >
+                                                                    <path
+                                                                        d="M5.63605 7.75735L7.05026 6.34314L12.7071 12L7.05029 17.6568L5.63608 16.2426L9.87869 12L5.63605 7.75735Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                    <path
+                                                                        d="M12.7071 6.34314L11.2929 7.75735L15.5356 12L11.2929 16.2426L12.7072 17.6568L18.364 12L12.7071 6.34314Z"
+                                                                        fill="currentColor"
+                                                                    ></path>
+                                                                </svg>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="small-rtc-box5">
+                                                    <div className="box1">
+                                                        <button style={{
+                                                            cursor:'pointer',
+                                                            color:'blue',
+                                                            backgroundColor:'transparent'
+                                                        }}>
+                                                            Add address
+                                                        </button>
+                                                        <a href="#">
+                                                            Download updated CSV file
+                                                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"  height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M19 9h-4V3H9v6H5l7 7 7-7zm-8 2V5h2v6h1.17L12 13.17 9.83 11H11zm-6 7h14v2H5z"></path></svg>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                             </div>
 
                                     {/* address details container */}
                                     <Modal isOpen={isModalOpen} onClose={closeModal}>
@@ -347,6 +546,15 @@ export default function Vesting() {
                                                         </div>
                                                     </div>
 
+                                                    <div className="toggle-middle-container">
+
+                                                              <div className="toggle-button-box">
+                                                              <input type="checkbox" id='check'/>
+                                                              <label htmlFor="check" className='button'></label>
+                                                    </div>
+
+                                                    <span style={{fontSize:'1em',fontWeight:'500',color:'black'}}>Cliff period <svg stroke="gray" fill="gray" stroke-width="0" viewBox="0 0 24 24" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"></path></svg></span>
+                                                    </div>
                                                     <div className="ad-box2">
                                                         <div>
                                                             <span>Cliff length (in months)</span>
